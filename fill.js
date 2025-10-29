13944274a97f355c21bfe625c48463f4cc46046f
bilal <bilalbakessou@gmail.com>
2025-10-29 10:40:52 +0100

    html + css

---
+++ fill.js
@@ -0,0 +1,21 @@
+
+let cont =0;
+function handleClick(elm){
+      
+      let imgName =elm.getAttribute('data-img');
+      elm.getAttribute("smya");
+
+      cont++;
+      if(cont ==1){
+            let img_1=imgName;
+      }else{
+           
+      }
+      
+      
+      if(cont==2){
+          if
+      }
+
+      alert(imgName);
+}
